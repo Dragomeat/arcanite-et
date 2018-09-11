983fcdc9bd8785522032c710e327bdcc6fc6a251
Artem Prosvetov <dragomeat@dragomeat.com>
2018-09-11 13:20:05 +0300

    Add sentry for capture exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,53 +1,38 @@
 <?php
+/**
+ * This file is part of the arcanite-et package.
+ *
+ *  (c) Artem Prosvetov <dragomeat@dragomeat.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
 
 namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
+/**
+ * Class Handler
+ */
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of the exception types that are not reported.
+     * @param Exception $exception
      *
-     * @var array
-     */
-    protected $dontReport = [
-        //
-    ];
-
-    /**
-     * A list of the inputs that are never flashed for validation exceptions.
-     *
-     * @var array
-     */
-    protected $dontFlash = [
-        'password',
-        'password_confirmation',
-    ];
-
-    /**
-     * Report or log an exception.
-     *
-     * @param \Exception $exception
+     * @throws Exception
      *
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Exception $exception): void
     {
-        parent::report($exception);
-    }
+        if ($this->shouldReport($exception)) {
+            app('sentry')->captureException($exception);
+        }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param \Exception               $exception
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function render($request, Exception $exception)
-    {
-        return parent::render($request, $exception);
+        parent::report($exception);
     }
 }
